e342ffedfce3fb8b84c258a352c8d1a0c9fd276d
HGoyena <89911703+HGoyena@users.noreply.github.com>
2021-09-20 08:40:50 +0200

    Update graphs.r

--- R/graphs.r
+++ R/graphs.r
@@ -1,15 +1,38 @@
 require(graphics)
 library(raster)
 library(mapview)
-###############
+library(ncdf4)
+###################
 # plot locations in map
-###############
+###################
+nc_data <- nc_open('Data/wind_residual.nc')
+lon <- ncvar_get(nc_data, "lon")
+lat <- ncvar_get(nc_data, "lat", verbose = F)
 # original data
-loc73 <- c(73, 36.8419, 26.0991)
-loc2137 <- c(2137, 48.3264, 20.8656)
-data <- rbind(loc73,loc2137)
-loc.spdf <- SpatialPointsDataFrame(data[,c(2,3)], data.frame(data[,1]), proj4string = CRS('+proj=longlat'))
-mapview(loc.spdf)
+olon <- lon[c(1:72,74:2136,2138:3173)]
+olat <- lat[c(1:72,74:2136,2138:3173)]
+on <- rep(NA,3171)
+loc73 <- c(73, lon[73], lat[73])
+loc2137 <- c(2137, lon[2137], lat[2137])
+
+dataN <- cbind(on,olon,olat)
+dataU <- rbind(loc73,loc2137)
+data <- rbind(dataN,dataU)
+loc.spdfN <- SpatialPointsDataFrame(dataN[,c(2,3)], data.frame(dataN[,1]),
+                                   proj4string = CRS('+proj=longlat')
+                                   )
+loc.spdfU <- SpatialPointsDataFrame(dataU[,c(2,3)], data.frame(dataU[,1]),
+                                    proj4string = CRS('+proj=longlat')
+)
+mapview(loc.spdfN
+        ,cex = 1
+        ,col.regions=c("grey")
+        # ,alpha.regions = 0.01
+        ,alpha = 0.1
+        )+ mapview(loc.spdfU
+          # ,cex = 3
+          ,alpha = 1
+)
 
 ###############
 # 73 location mean wind speed
@@ -41,22 +64,22 @@ for (i in c(1:4)) {
 
 par(mfrow = c(2,2))
 
-plot(x=c(1:24),sds[1,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'Año 2013',col='red', ylim=c(-0.5,1.5))
+plot(x=c(1:24),sds[1,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'AÃ±o 2013',col='red', ylim=c(-0.5,1.5))
 points(x=c(1:24),sds[1,],pch = 23, col = 'red')
 lines(x=c(1:24),means[1,], col = 'blue')
 points(x=c(1:24),means[1,],pch = 24, col = 'blue')
 
-plot(x=c(1:24),sds[2,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'Año 2014',col='red', ylim=c(-0.5,1.5))
+plot(x=c(1:24),sds[2,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'AÃ±o 2014',col='red', ylim=c(-0.5,1.5))
 points(x=c(1:24),sds[2,],pch = 23, col = 'red')
 lines(x=c(1:24),means[2,], col = 'blue')
 points(x=c(1:24),means[2,],pch = 24, col = 'blue')
 
-plot(x=c(1:24),sds[3,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'Año 2015',col='red', ylim=c(-0.5,1.5))
+plot(x=c(1:24),sds[3,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'AÃ±o 2015',col='red', ylim=c(-0.5,1.5))
 points(x=c(1:24),sds[3,],pch = 23, col = 'red')
 lines(x=c(1:24),means[3,], col = 'blue')
 points(x=c(1:24),means[3,],pch = 24, col = 'blue')
 
-plot(x=c(1:24),sds[4,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'Año 2016',col='red', ylim=c(-0.5,1.5))
+plot(x=c(1:24),sds[4,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'AÃ±o 2016',col='red', ylim=c(-0.5,1.5))
 points(x=c(1:24),sds[4,],pch = 23, col = 'red')
 lines(x=c(1:24),means[4,], col = 'blue')
 points(x=c(1:24),means[4,],pch = 24, col = 'blue')
@@ -90,22 +113,22 @@ for (i in c(1:4)) {
   i = i+1
 }
 
-plot(x=c(1:24),sds[1,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'Año 2013',col='red', ylim=c(-0.5,1.5))
+plot(x=c(1:24),sds[1,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'AÃ±o 2013',col='red', ylim=c(-0.5,1.5))
 points(x=c(1:24),sds[1,],pch = 23, col = 'red')
 lines(x=c(1:24),means[1,], col = 'blue')
 points(x=c(1:24),means[1,],pch = 24, col = 'blue')
 
-plot(x=c(1:24),sds[2,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'Año 2014',col='red', ylim=c(-0.5,1.5))
+plot(x=c(1:24),sds[2,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'AÃ±o 2014',col='red', ylim=c(-0.5,1.5))
 points(x=c(1:24),sds[2,],pch = 23, col = 'red')
 lines(x=c(1:24),means[2,], col = 'blue')
 points(x=c(1:24),means[2,],pch = 24, col = 'blue')
 
-plot(x=c(1:24),sds[3,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'Año 2015',col='red', ylim=c(-0.5,1.5))
+plot(x=c(1:24),sds[3,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'AÃ±o 2015',col='red', ylim=c(-0.5,1.5))
 points(x=c(1:24),sds[3,],pch = 23, col = 'red')
 lines(x=c(1:24),means[3,], col = 'blue')
 points(x=c(1:24),means[3,],pch = 24, col = 'blue')
 
-plot(x=c(1:24),sds[4,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'Año 2016',col='red', ylim=c(-0.5,1.5))
+plot(x=c(1:24),sds[4,],type = 'l', xlab = 'Hora', ylab = 'Residual', main = 'AÃ±o 2016',col='red', ylim=c(-0.5,1.5))
 points(x=c(1:24),sds[4,],pch = 23, col = 'red')
 lines(x=c(1:24),means[4,], col = 'blue')
 points(x=c(1:24),means[4,],pch = 24, col = 'blue')
